c299482d95f29ffab530599e1ef49cf8e710e959
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2026-04-05 16:25:51 +0200

    fix(errors): correctly use `super()`

--- svg2png/errors.py
+++ svg2png/errors.py
@@ -2,5 +2,5 @@ from pathlib import Path
 from typing import NoReturn
 
 class InvalidSVGError(Exception):
-	def __init__(self, path_or_content: str | Path) -> NoReturn:
-		super(f"Invalid SVG: {path_or_content}")
+	def __init__(self, input_code: str) -> NoReturn:
+		super().__init__(f"Invalid SVG: {input_code}")
