85c1fe26dde77da1a1445238a0e81a1bc497c4c4
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2026-04-05 16:26:23 +0200

    chore(renderer): docstring

--- svg2png/renderer.py
+++ svg2png/renderer.py
@@ -3,4 +3,16 @@ from lxml import etree
 from .errors import InvalidSVGError
 
 def render(input_code: str) -> bytes:
-	pass
+	"""Render SVG code into PNG binary data
+
+	Args:
+		input_code (str): SVG code
+
+	Returns:
+		bytes: PNG binary data
+
+	Raises:
+		InvalidSVGError: invalid SVG code
+	"""
+
+	raise InvalidSVGError(input_code)
