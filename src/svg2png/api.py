a85de436f3ae54e87ec49a89dd71a6ad9f97c2e5
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2026-04-07 02:59:56 +0200

    feat(API): pass down input SVG path to `render()` for error messages

--- src/svg2png/api.py
+++ src/svg2png/api.py
@@ -43,7 +43,7 @@ def svg_path2png_path(input_path: Path, output_path: Optional[Path]) -> None:
 	with open(input_path, 'r') as input_file:
 		input_code = input_file.read()
 	
-	svg_code2png_path(input_code, output_path)
+	svg_code2png_path(input_code, output_path, input_path=input_path)
 
 def svg_path2png_bin(input_path: Path) -> bytes:
 	"""Render SVG file and return PNG as bytes
@@ -58,25 +58,27 @@ def svg_path2png_bin(input_path: Path) -> bytes:
 	with open(input_path, 'r') as input_file:
 		input_code = input_file.read()
 	
-	return svg_code2png_bin(input_code)
+	return svg_code2png_bin(input_code, input_path=input_path)
 
 
-def svg_code2png_path(input_code: str, output_path: Path) -> None:
+def svg_code2png_path(input_code: str, output_path: Path, input_path: Optional[Path] = None) -> None:
 	"""Render provided SVG code into a PNG file
 
 	Args:
 		input_code (str): SVG code
 		output_path (Path): output PNG's path
+		input_path (Optional[Path]): input SVG's path (for error messages)
 	"""
 	
 	with open(output_path, 'wb') as output_file:
-		output_file.write(svg_code2png_bin(input_code))
+		output_file.write(svg_code2png_bin(input_code, input_path=input_path))
 
-def svg_code2png_bin(input_code: str) -> bytes:
+def svg_code2png_bin(input_code: str, input_path: Optional[Path] = None) -> bytes:
 	"""Render provided SVG code and return PNG as bytes
 
 	Args:
 		input_code (str): SVG code
+		input_path (Optional[Path]): input SVG's path (for error messages)
 
 	Returns:
 		bytes: PNG data
