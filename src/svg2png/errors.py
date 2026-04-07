cade65f3fe9afee7384330ed355955cedf4fbae0
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2026-04-07 21:50:06 +0200

    fix(errors): type hints

--- src/svg2png/errors.py
+++ src/svg2png/errors.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from typing import NoReturn
+from typing import Optional
 
 class InvalidSVGError(Exception):
-	def __init__(self, input_code: str, input_path: Optional[Path] = None) -> NoReturn:
+	def __init__(self, input_code: str, input_path: Optional[Path] = None) -> None:
 		super().__init__(f'Invalid SVG "{input_code}"{f" in \"{input_path}\"" if input_path else ""}')
